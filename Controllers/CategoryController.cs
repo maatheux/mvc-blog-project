8e2a6a0512cebd1275c27b98120221829d15e25e
Matheus Lima <matheusmarinho.lima@hotmail.com>
2024-02-23 18:08:35 -0300

    Crud de categorias e tratamento de erro

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -12,8 +12,117 @@ namespace Blog.Controllers
         public async Task<IActionResult> GetAsync(
             [FromServices] BlogDataContext context)
         {
-            IList<Category> categories = await context.Categories.ToListAsync();
-            return Ok(categories);
+            try
+            {
+                IList<Category> categories = await context.Categories.ToListAsync();
+                return Ok(categories);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, "05XE04 Falha interna no servidor");
+            }
+        }
+
+        [HttpGet("v1/categories/{id:int}")]
+        public async Task<IActionResult> GetByIdAsync(
+            [FromServices] BlogDataContext context,
+            [FromRoute] int id)
+        {
+            try
+            {
+                Category? category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+
+                if (category == null)
+                    return NotFound();
+
+                return Ok(category);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, "05XE05 Falha interna no servidor");
+            }
+        }
+
+        [HttpPost("v1/categories")]
+        public async Task<IActionResult> PostAsync(
+            [FromServices] BlogDataContext context,
+            [FromBody] Category model)
+        {
+
+            try
+            {
+                await context.Categories.AddAsync(model);
+                await context.SaveChangesAsync();
+
+                return Created($"v1/categories/{model.Id}", model);
+            }
+            catch (DbUpdateException e)
+            {
+                // return BadRequest("Não foi inculir a categoria"); // qnd da requisicao invalida
+                return StatusCode(500, "05XE9 - Não foi possível inculir a categoria");
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, "05XE10 Falha interna no servidor");
+            }
+        }
+
+        [HttpPut("v1/categories/{id:int}")]
+        public async Task<IActionResult> PutAsync(
+            [FromServices] BlogDataContext context,
+            [FromRoute] int id,
+            [FromBody] Category model)
+        {
+            try
+            {
+                Category? category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+
+                if (category == null)
+                    return NotFound();
+
+                category.Name = model.Name;
+                category.Slug = model.Slug;
+
+                context.Categories.Update(category);
+                await context.SaveChangesAsync();
+
+                return Ok(model);
+            }
+            catch (DbUpdateException e)
+            {
+                return StatusCode(500, "05XE8 - Não foi possível alterar a categoria");
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, "05XE11 Falha interna no servidor");
+            }
+        }
+
+        [HttpDelete("v1/categories/{id:int}")]
+        public async Task<IActionResult> DeleteAsync(
+            [FromServices] BlogDataContext context,
+            [FromRoute] int id)
+        {
+            try
+            {
+                Category? category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+
+                if (category == null)
+                    return NotFound();
+
+                context.Categories.Remove(category);
+                await context.SaveChangesAsync();
+
+                return Ok(category);
+            }
+            catch (DbUpdateException e)
+            {
+                return StatusCode(500, "05XE7 - Não foi possível deletar a categoria");
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, "05XE12 Falha interna no servidor");
+            }
         }
     }
 }
