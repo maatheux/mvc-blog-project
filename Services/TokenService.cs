8647caaf1b42e2c57a037d4f2b91062afff80fa5
Matheus Lima <matheusmarinho.lima@hotmail.com>
2024-03-01 15:33:11 -0300

    Gerando tokens com Claims

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,6 +1,7 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using Blog.Extensions;
 using Blog.Models;
 using Microsoft.IdentityModel.Tokens;
 
@@ -10,30 +11,23 @@ public class TokenService
 {
     public string GenerateToken(User user)
     {
-        var tokenHandler = new JwtSecurityTokenHandler(); // manipulador do token
-        var key = Encoding.ASCII.GetBytes(Configuration.JwtKey); // a chave tem q ser passada como uma array de bytes
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+        var claims = user.GetClaims();
+        
         var tokenDescriptor = new SecurityTokenDescriptor
         {
-            Subject = new ClaimsIdentity(new Claim[]
-            {
-                new (ClaimTypes.Name, "andrebaltieri"), // acesso pelo User.Identity.Name
-                new (ClaimTypes.Role, "admin"), // User.IsInRole()
-                new (ClaimTypes.Role, "user"),
-                new ("fruta", "banana")
-            }), // attr para setar os claims (afirmacaoes sobre o token / objetos chave-valor). E o ASP.NET ja possui alguns tipos de claim
+            Subject = new ClaimsIdentity(claims),
             Expires = DateTime.UtcNow.AddHours(8),
             SigningCredentials = new SigningCredentials
             (
                 new SymmetricSecurityKey(key), 
                 SecurityAlgorithms.HmacSha256Signature
-            ) // como token sera gerado/lido/(des)encriptografado
+            )
             
-        }; // ira ter todas as info do token
+        };
         var token = tokenHandler.CreateToken(tokenDescriptor);
         
-        return tokenHandler.WriteToken(token); // converte o token para uma string
+        return tokenHandler.WriteToken(token);
     }
 }
-
-// dotnet add package Microsoft.AspNetCore.Authentication
-// dotnet add package Microsoft.AspNetCore.Authentication.JwtBearer  --> bearer - tipo de autenticacao do jwt
--- obj/Debug/net8.0/Blog.AssemblyInfo.cs
+++ obj/Debug/net8.0/Blog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0dc2762d7e4b8bf2df352f9b4f00b254e24e143")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31a7eda677e7d7c4f5093b20ff41f1d95b1a728c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
