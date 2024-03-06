90103233d8c9bef9dbb4b2979ab46c73e7a286e3
Matheus Lima <matheusmarinho.lima@hotmail.com>
2024-03-06 17:39:17 -0300

    Organizando o program

--- Program.cs
+++ Program.cs
@@ -6,42 +6,14 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+ConfigureAuthentication(builder);
+ConfigureMvc(builder);
+ConfigureServices(builder);
 
-var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
-builder.Services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; // autenticacao padrao Bearer
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; // token/autenticacao gerados neste servidor
-}).AddJwtBearer(x =>
-{
-    x.TokenValidationParameters = new TokenValidationParameters()
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});
-
-builder.Services
-    .AddControllers()
-    .ConfigureApiBehaviorOptions(options =>
-    {
-        options.SuppressModelStateInvalidFilter = true;
-    });
-builder.Services.AddDbContext<BlogDataContext>();
-
-builder.Services.AddTransient<TokenService>();
 
 var app = builder.Build();
 
-Configuration.JwtKey = app.Configuration.GetValue<string>("JwtKey");
-Configuration.ApiKey = app.Configuration.GetValue<string>("ApiKey");
-Configuration.ApiKeyName = app.Configuration.GetValue<string>("ApiKeyName");
-
-var smtp = new Configuration.SmtpConfiguration();
-app.Configuration.GetSection("Smtp").Bind(smtp); // ira pegar a section (objeto smtp no json) e converter para a classe
-Configuration.Smtp = smtp;
+LoadConfiguration(app);
 
 app.UseAuthentication();
 app.UseAuthorization();
@@ -49,3 +21,49 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void LoadConfiguration(WebApplication webApp)
+{
+    Configuration.JwtKey = webApp.Configuration.GetValue<string>("JwtKey");
+    Configuration.ApiKey = webApp.Configuration.GetValue<string>("ApiKey");
+    Configuration.ApiKeyName = webApp.Configuration.GetValue<string>("ApiKeyName");
+
+    var smtp = new Configuration.SmtpConfiguration();
+    webApp.Configuration.GetSection("Smtp").Bind(smtp); // ira pegar a section (objeto smtp no json) e converter para a classe
+    Configuration.Smtp = smtp;
+}
+
+void ConfigureAuthentication(WebApplicationBuilder webAppBuilder)
+{
+    var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+    webAppBuilder.Services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; // autenticacao padrao Bearer
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; // token/autenticacao gerados neste servidor
+    }).AddJwtBearer(x =>
+    {
+        x.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+}
+
+void ConfigureMvc(WebApplicationBuilder webAppBuilder)
+{
+    webAppBuilder.Services
+        .AddControllers()
+        .ConfigureApiBehaviorOptions(options =>
+        {
+            options.SuppressModelStateInvalidFilter = true;
+        });
+}
+
+void ConfigureServices(WebApplicationBuilder webAppBuilder)
+{
+    webAppBuilder.Services.AddDbContext<BlogDataContext>();
+    webAppBuilder.Services.AddTransient<TokenService>();
+}
--- obj/Debug/net8.0/Blog.AssemblyInfo.cs
+++ obj/Debug/net8.0/Blog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b25f14129c0fd12f2364487fe5b932d776272738")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c623fe3b00a302c49089f0fec065c6e02336200")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
